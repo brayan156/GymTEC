767aecb2e071b16f7565717c8b82d07156a6680b
JUARSO <juan09fut.js@gmail.com>
2021-06-01 21:16:26 -0600

    Varias  vistas terminadas

---
+++ FrontEnd/src/app/Administrador/gestion-tipo-planilla/gestion-tipo-planilla.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-tipo-planilla',
+  templateUrl: './gestion-tipo-planilla.component.html',
+  styleUrls: ['./gestion-tipo-planilla.component.css']
+})
+export class GestionTipoPlanillaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
