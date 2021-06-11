0d1b6a6a32e17f7c01cab940bea86ea5d92f2feb
Brayan <brayanleon25@hotmail.com>
2021-06-11 14:39:15 -0600

    vistas controller

---
+++ Backend/Gymtec_API/Controllers/tratamientos_asociadosController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Gymtec_API;
+
+namespace Gymtec_API.Controllers
+{
+    public class tratamientos_asociadosController : ApiController
+    {
+        private gymtecdatabaseEntities1 db = new gymtecdatabaseEntities1();
+
+        // GET: api/tratamientos_asociados
+        public IQueryable<tratamientos_asociados> Gettratamientos_asociados()
+        {
+            return db.tratamientos_asociados;
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool tratamientos_asociadosExists(int id)
+        {
+            return db.tratamientos_asociados.Count(e => e.cedula == id) > 0;
+        }
+    }
+}
